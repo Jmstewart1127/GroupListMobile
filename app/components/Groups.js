c2d4f00e25a08ff086229cf9b58b8959b65930b2
Jacob Stewart <jmstewart1127@gmail.com>
2018-08-27 21:15:46 -0500

    added project

---
+++ app/components/Groups.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, List, ListItem, Text, Icon, Right } from 'native-base';
+export default class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: [],
+    }
+  }
+
+  getUsersGroups = id => {
+    fetch('https://group-list-api.herokuapp.com/api/get/my/groups/' + id)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({
+          groups: responseJson,
+        });
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  componentDidMount() {
+    this.getUsersGroups(1);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card>
+            <CardItem>
+              <List>
+                {this.state.groups.map((group) => {
+                  return(
+                    <ListItem>
+                      <Text>{group.groupName}</Text>
+                    </ListItem>
+                  );
+                })}
+              </List>
+             </CardItem>
+           </Card>
+        </Content>
+      </Container>
+    );
+  }
+}
